e4cf0a9d89723c1fee8630cfe9c3175f03540848
mac <mac@macs-MacBook-Pro.local>
2024-12-01 22:12:24 +0100

    Index Performance

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -3,3 +3,9 @@
 CREATE INDEX Id_fullName ON USER (first_name, last_name);
 
 CREATE INDEX Id_PropName on Property(name);
+
+-- Using EXPLAIN and ANALYZE
+
+EXPLAIN SELECT USER.first_name, USER.last_name from USER;
+
+EXPLAIN ANALYZE SELECT Property.name FROM Property;
