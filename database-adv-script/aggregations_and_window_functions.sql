1d4777a1bdba7a113155cb1a7f4ebd69e8985464
mac <mac@macs-MacBook-Pro.local>
2024-12-01 21:45:05 +0100

    Aggregation

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -8,7 +8,7 @@ Booking
 ON Booking.user_id = USER.user_id
 GROUP BY USER.user_id;
 
--- A window function (ROW_NUMBER, RANK) to rank properties based on the total number of bookings they have received.
+-- A window function (RANK) to rank properties based on the total number of bookings they have received.
 
 SELECT Property.name, COUNT(Booking.booking_id),
 RANK() OVER (ORDER BY COUNT(Booking.booking_id) DESC) AS rank;
@@ -17,3 +17,13 @@ INNER JOIN Booking
 ON Booking.property_id = Property.property_id
 GROUP BY Property.property_id, Property.name
 ORDER BY rank;
+
+
+-- A window function (ROW_NUMBER) to rank properties based on the total number of bookings they have received.
+SELECT Property.name, COUNT(Booking.booking_id),
+ROW_NUMBER() OVER (ORDER BY COUNT(Booking.booking_id) DESC) AS row_numnber;
+FROM Property
+INNER JOIN Booking
+ON Booking.property_id = Property.property_id
+GROUP BY Property.property_id, Property.name
+ORDER BY row_numnber;
