0db9ca2a7f8634091f88cf1c8b6c93f0c14fb2c6
mac <mac@macs-MacBook-Pro.local>
2024-12-01 22:57:49 +0100

    Index Performance

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -5,7 +5,9 @@ SELECT Booking.booking_id, Property.name, CONCAT(USER.first_name, ' ', USER.last
 FROM Booking
 LEFT JOIN USER ON Booking.user_id = USER.user_id 
 LEFT JOIN Property ON Property.property_id = Booking.property_id
-LEFT JOIN Payment ON payment.booking_id = Booking.booking_id;
+LEFT JOIN Payment ON payment.booking_id = Booking.booking_id
+WHERE Booking.booking_id IS NOT NULL
+AND Payment.amount > 0;
 
 -- USING EXPLAIN
 
@@ -15,3 +17,4 @@ FROM Booking
 LEFT JOIN USER ON Booking.user_id = USER.user_id 
 LEFT JOIN Property ON Property.property_id = Booking.property_id
 LEFT JOIN Payment ON payment.booking_id = Booking.booking_id;
+
