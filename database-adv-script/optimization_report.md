97f9ac88fe49d2020f34ae280759b515fd8b6f73
mac <mac@macs-MacBook-Pro.local>
2024-12-01 22:40:17 +0100

    Index Performance

---
+++ database-adv-script/optimization_report.md
@@ -0,0 +1,3 @@
+# OPTIMIZATION OF THE QUERIES MADE
+## By creating index for most frequent column being queried will increase the performance of the query
+## Doing this before joining the table is way better.
---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,17 @@
+-- An initial query that retrieves all bookings along with the user details, property details, and payment details and save it on perfomance.sql
+
+SELECT Booking.booking_id, Property.name, CONCAT(USER.first_name, ' ', USER.last_name),
+ Property.description, Payment.amount, Payment.payment_method
+FROM Booking
+LEFT JOIN USER ON Booking.user_id = USER.user_id 
+LEFT JOIN Property ON Property.property_id = Booking.property_id
+LEFT JOIN Payment ON payment.booking_id = Booking.booking_id;
+
+-- USING EXPLAIN
+
+EXPLAIN SELECT Booking.booking_id, Property.name, CONCAT(USER.first_name, ' ', USER.last_name),
+ Property.description, Payment.amount, Payment.payment_method
+FROM Booking
+LEFT JOIN USER ON Booking.user_id = USER.user_id 
+LEFT JOIN Property ON Property.property_id = Booking.property_id
+LEFT JOIN Payment ON payment.booking_id = Booking.booking_id;
